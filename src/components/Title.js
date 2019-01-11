66360eec919aa92940f00c40d4c17baceb7bdf38
佐々木 拓真 <sasaki.takuma@jp.fujitsu.com>
2019-01-11 15:40:45 +0900

    add Title component

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+// 外部のライブラリやファイルを参照するインポートの部分
+import React from 'react';
+
+// JSXやメソッドを定義する実質上の中身
+const Title = () => {
+ return(
+   <div>
+     <h2>Hello World 2.0</h2>
+   </div>
+ )
+}
+
+// 外部のファイルがこのcomponentを利用できるようにするエクスポートの部分
+export default Title;
